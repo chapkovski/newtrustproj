42827c2d3f8144b9ceba1c6570a8090cb3526c14
chapkovski <chapkovski@gmail.com>
2020-02-29 15:47:04 -0500

    removing apps.py

--- trust/apps.py
+++ trust/apps.py
@@ -6,11 +6,11 @@ from django.db.utils import OperationalError
 class TrustConfig(AppConfig):
     name = 'trust'
 
-    def ready(self):
-        print('READY?')
-        from .models import City
-        try:
-            for i in settings.CITIES:
-                City.objects.get_or_create(code=i['code'], defaults={'description': i['name']})
-        except OperationalError:
-            print('no table is ready yet...')
+    # def ready(self):
+    #     print('READY?')
+    #     from .models import City
+    #     try:
+    #         for i in settings.CITIES:
+    #             City.objects.get_or_create(code=i['code'], defaults={'description': i['name']})
+    #     except OperationalError:
+    #         print('no table is ready yet...')
