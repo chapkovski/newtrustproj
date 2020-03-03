599b76548e900bfc0e0b95cf2b0e0432858a5743
albelix <albelix@yandex.ru>
2020-03-03 21:28:38 +0300

    translated mostly

--- trust/cq_models.py
+++ trust/cq_models.py
@@ -25,7 +25,7 @@ correct_answers = dict(
     cq2_2=10,
     cq2_3=0,
     cq2_4=10,
-    cq2_5=20,
+    cq2_5=0,
 )
 
 
@@ -34,7 +34,6 @@ class CQ(models.IntegerField):
         if hasattr(self, 'correct_answer'):
             if value != correct_answers[self.correct_answer]:
                 raise ValidationError(f'Please check this answer')
-
 
     def __init__(self, *args, **kwargs):
         if 'correct_answer' in kwargs:
@@ -45,12 +44,12 @@ class CQ(models.IntegerField):
         super().__init__(*args, **kwargs)
 
 
-cq_1_8_choices = ((1, 'None'),
-                  (2, '12 - one per each city that is involved in this study.'),
-                  (3, '1 - a randomly chosen participant from one of the 12 cities that are involved in this study. '))
-cq_1_9_choices = ((1, '12 - one per each city that is involved in this study.'),
-                  (2, '1 - a randomly chosen participant from one of the 12 cities that are involved in this study. '),
-                  (3, 'None')
+cq_1_8_choices = ((1, 'Ни для кого'),
+                  (2, 'Для 12 – по одному из каждого города, участвующего в исследовании.'),
+                  (3, 'Для 1 случайно выбранного участника из одного из 12 городов, участвующих в исследовании.'))
+cq_1_9_choices = ((1, '12 – по одному из каждого города, участвующего в исследовании.'),
+                  (2, '1 – случайно выбранный участник из одного из 12 городов, принимающих участие в исследовании.  '),
+                  (3, 'Ни один не примет')
                   )
 
 
@@ -59,45 +58,46 @@ class CQPlayer(BasePlayer):
         abstract = True
 
     # cqs for part 1
-    cq1_1 = CQ(label='If Person A transfers his initial endowment to Person B, how many token does Person B receive? ',
+    cq1_1 = CQ(label='Если Участник А передаст свою начальную сумму Участнику Б, сколько токенов получит Участник Б?',
                correct_answer='cq1_1')
     cq1_2 = CQ(
-        label='A Person A transfers his initial endowment to Person B. What is the maximum amount Person B can return to Person A? ',
+        label='Участник А передал свою начальную сумму Участнику Б. Какую максимальную сумму может Участник Б вернуть обратно Участнику А? ',
         correct_answer='cq1_2')
-    cq1_3 = CQ(label='Person B decides to return to Person A 20 points. How many points does Person A receive from this transfer?',
-               correct_answer='cq1_3')
+    cq1_3 = CQ(
+        label='Участник Б решает вернуть Участнику А 20 токенов. Сколько токенов получит Участник А от участника Б?',
+        correct_answer='cq1_3')
     cq1_4 = CQ(
-        label='What is the payoff of Person B if Person A transfers his initial endowment and Person B sends back 9 points?'
+        label='Сколько токенов получит Участник Б всего за часть 1 эксперимента, если Участник А передаст ему свою начальную сумму, а Участник Б вернет Участнику А 9 токенов?'
         , correct_answer='cq1_4')
     cq1_5 = CQ(
-        label='What is the payoff of Person A if he transfers his initial endowment and Person B sends him back 9 points?',
+        label='Сколько токенов получит Участник А по итогам эксперимента если он передаст Участнику Б свою начальную сумму, а Участник Б вернет Участнику А 9 токенов?',
         correct_answer='cq1_5')
     cq1_6 = CQ(
-        label='What is the payoff of Person A if Person A decides NOT to transfer his initial endowment to Person B? ',
+        label='Сколько токенов получит Участник А, если он решит не передавать Участнику Б свою начальную сумму? ',
         correct_answer='cq1_6')
     cq1_7 = CQ(
-        label='What is the payoff of Person B if Person A decides NOT to transfer his initial endowment to Person B? ',
+        label='Сколько токенов получит Участник Б, если Участник А решит не передавать Участнику Б свою начальную сумму? ',
         correct_answer='cq1_7')
     cq1_8 = CQ(
-        label='Imagine you have been randomly assigned to be Person A. For how many participants in the role of Person B your decision to transfer or not transfer your initial endowment will be payoff relevant?',
+        label='Предположим, Вам случайным образом выпала роль Участника А. Для скольких Участников Б Ваше решение о передаче своей начальной суммы будет определяющим для получения его выигрыша?',
         choices=cq_1_8_choices, correct_answer='cq1_8', widget=widgets.RadioSelect)
     cq1_9 = CQ(
-        label='Imagine you have been randomly assigned to be Person B. How many participants in the role of Person A will decide on whether you actually receive a multiplied initial endowment?',
+        label='Предположим, Вам случайным образом выпала роль Участника Б. Сколько Участников А примут решение о том, получите ли вы их утроенную начальную сумму?',
         choices=cq_1_9_choices, correct_answer='cq1_9', widget=widgets.RadioSelect)
 
     # cqs for part 1
     cq2_1 = CQ(
-        label=' If Person A from City 1 transferred his initial endowment, while the estimate of Person B was that a person from City 1 will not transfer the endowment, what will be the payoff of Person B for Part 2? ',
+        label='В части 1 Участник А из города 1 передал свою начальную сумму Участнику Б. Участника Б из города 1, с которым взаимодействовал этот Участник А, дал оценку что Участник Б из его пары не передаст ему начальную сумму. Сколько токенов получит за эту оценку в части 2 этот Участник Б?',
         correct_answer='cq2_1')
     cq2_2 = CQ(
-        label='If Person A from City 1 transferred his initial endowment. Person B’s estimate for a Person A from City 1 was that this person transfers his endowment. What will be the payoff of Person B for Part 2?',
+        label='В части 1 Участник А из города 1 передал свою начальную сумму Участнику Б. Участника Б из города 1, с которым взаимодействовал этот Участник А, дал оценку что Участник Б из его пары передаст ему начальную сумму. Сколько токенов получит за эту оценку в части 2 этот Участник Б?',
         correct_answer='cq2_2')
     cq2_3 = CQ(
-        label='In Part 1, a Person A from City 1 transferred his initial endowment to a Person B from City 2. Person B returns 15 tokens to Person A in Part 1. In Part 2, Person A estimated that Person B returns 9 tokens.  What will be the payoff of Person A for Part 2? ',
+        label='В части 1 Участник А из города 1 передал свою начальную сумму Участнику Б из города 2. Участник Б из города 2, с которым взаимодействовал этот Участник А, вернул ему 15 токенов. Участник А в части 2 дал оценку, что этот Участник Б вернул ему 9 токенов. Сколько токенов получит Участник А из этой пары за такую оценку?',
         correct_answer='cq2_3')
     cq2_4 = CQ(
-        label=' In Part 1, a Person A from City 1 did not transfer his initial endowment to a Person B from City 2. Person B from City 2 chose to return 15 tokens to Person A if Person A transfers his initial endowment. In Part 2, Person A estimated that Person B would return 18 tokens.  What will be the payoff of Person A for Part 2?',
+        label='В части 1 Участник А из города 1 не передал свою начальную сумму Участнику Б из города 2. Участник Б из города 2, с которым взаимодействовал этот Участник А, был готов вернуть этому Участнику А 15 токенов в том случае, если бы тот передал ему свою начальную сумму. Участник А в части 2 дал оценку, что этот Участник Б был готов вернуть ему 18 токенов. Сколько токенов получит Участник А из этой пары за такую оценку?',
         correct_answer='cq2_4')
     cq2_5 = CQ(
-        label='In Part 1, a Person A from City 1 transferred his initial endowment to a Person B from City 2. Person B from City 2 chose to return 0 tokens to Person A if Person A transfers his initial endowment. In Part 2, Person A estimated that Person B would return 0 tokens. What will be the payoff of Person A for Part 2?',
+        label='В части 1 Участник А из города 1 передал свою начальную сумму Участнику Б из города 2. Участник Б из города 2, с которым взаимодействовал этот Участник А, решил ничего не возвращать этому Участнику А в том случае, если бы тот передал ему свою начальную сумму. Участник А в части 2 дал оценку, что этот Участник Б был готов вернуть ему 9 токенов. Сколько токенов получит Участник А из этой пары за такую оценку? ',
         correct_answer='cq2_5')
