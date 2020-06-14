0a54a2e33e04977752310ff79aa35978804e6367
chapkovski <chapkovski@gmail.com>
2020-06-13 22:19:06 -0400

    moving fowrad with tests

--- tests/test_mingle.py
+++ tests/test_mingle.py
@@ -37,5 +37,6 @@ class AnimalTestCase(TestCase):
         print(MegaParticipant.objects.all().count())
 
         m.form_groups()
+        print('MMMM', m.megagroups.all().count())
         # m.calculate_payoffs()
 
