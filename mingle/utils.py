f2426a24d436b97c9d9e600f0266193c8caa3ace
chapkovski <chapkovski@gmail.com>
2020-06-29 08:55:32 -0400

    before potential reshuffle of decisions

--- mingle/utils.py
+++ mingle/utils.py
@@ -6,11 +6,8 @@ def time_check(func):
     def wrapper(*args, **kwargs):
         startq = len(connection.queries)
         start_time = time()
-        print("Something is happening before the function is called.")
         func(*args, **kwargs)
-        print("Something is happening after the function is called.")
         endq = len(connection.queries)
-
         print(f'FUNC: {func.__name__}; Total queries: {endq - startq}')
         print(f'FUNC: {func.__name__}; Total time: {   time() - start_time}')
 
