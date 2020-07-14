5d0930bab7b9ba70ec25ef98302d589072a5a9c6
chapkovski <chapkovski@gmail.com>
2020-07-13 21:48:57 -0400

    upd part fee to 0

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ GOOGLE_API_KEY = environ.get('GOOGLE_API_KEY')
 print('GOOGLE_API', GOOGLE_API_KEY)
 SESSION_CONFIG_DEFAULTS = dict(
     real_world_currency_per_point=0.10,
-    participation_fee=150.00,
+    participation_fee=0,
     doc="",
     use_browser_bots=False,
     debug=False,
