97344590880f8da12b038ec7cc367103365acb04
chapkovski <chapkovski@gmail.com>
2020-07-19 20:15:41 -0400

    upd management

--- mingle/management/commands/create_tsessions.py
+++ mingle/management/commands/create_tsessions.py
@@ -2,26 +2,26 @@ from django.core.management.base import BaseCommand
 import logging
 from otree.session import create_session
 from trust.models import City
+
 logger = logging.getLogger(__name__)
 
-def session_creator(num_participants, stub):
-    cities = City.objects.all().order_by('code').values_list('code',flat=True)
 
+def session_creator(num_participants, stub):
+    cities = City.objects.all().order_by('code')
     for x in cities:
-        logger.info(f'Creating session for city: {x}; participants:{num_participants} ')
+        logger.info(f'Creating session for city: {x.eng}; participants:{num_participants} ')
         s = create_session(
             session_config_name='full_ru',
             num_participants=num_participants,
-            label=f'{stub}: {x}',
-            modified_session_config_fields=dict(city_code=x,
+            label=f'{stub}: {x.eng}',
+            modified_session_config_fields=dict(city_code=x.code,
                                                 toloka=True,
                                                 toloka_sandbox=False,
                                                 )
         )
         s.comment = s.label
-
         s.save()
-        print('SSS', s.code)
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -29,5 +29,5 @@ class Command(BaseCommand):
 
     def handle(self, num_participants, *args, **options):
         logger.warning('This command will create a bunch of sessions for each city for toloka')
-        stub =  input("Enter stub : ")
+        stub = input("Enter stub : ")
         session_creator(num_participants, stub)
