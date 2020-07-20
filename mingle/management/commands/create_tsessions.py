0c3010bdfe5d51ddcb87b3b2cdcdf5aba0df002a
chapkovski <chapkovski@gmail.com>
2020-07-19 20:06:21 -0400

    creating sessions management tool

---
+++ mingle/management/commands/create_tsessions.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+import logging
+from otree.session import create_session
+from trust.models import City
+logger = logging.getLogger(__name__)
+
+def session_creator(num_participants, stub):
+    cities = City.objects.all().order_by('code').values_list('code',flat=True)
+
+    for x in cities:
+        logger.info(f'Creating session for city: {x}; participants:{num_participants} ')
+        s = create_session(
+            session_config_name='full_ru',
+            num_participants=num_participants,
+            label=f'{stub}: {x}',
+            modified_session_config_fields=dict(city_code=x,
+                                                toloka=True,
+                                                toloka_sandbox=False,
+                                                )
+        )
+        s.comment = s.label
+
+        s.save()
+        print('SSS', s.code)
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('num_participants', help='Number of participants per session.', type=int)
+
+    def handle(self, num_participants, *args, **options):
+        logger.warning('This command will create a bunch of sessions for each city for toloka')
+        stub =  input("Enter stub : ")
+        session_creator(num_participants, stub)
